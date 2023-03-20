195afb8cb4f83b2e4133829869b8a52145e6ba01
saravalentinal <latynsara41@gmail.com>
2023-03-20 22:31:25 +0100

    Frontend Skills Base lista

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit {
+
+  skills:any;
+
+  constructor(private dataPortfolio:PortfolioService) {}
+
+  ngOnInit(): void{
+    this.dataPortfolio.obtainData().subscribe(data => {
+      console.log(data);
+      this.skills = data.skills;
+    });
+  }
 
 }
